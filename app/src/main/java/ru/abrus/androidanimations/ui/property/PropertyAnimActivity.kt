bcfa6ae63d23d28d895f94c1e42c518aad36e958
abrusdev <abrusdev@gmail.com>
2021-03-23 22:10:41 +0500

    init property anim listener

--- app/src/main/java/ru/abrus/androidanimations/ui/property/PropertyAnimActivity.kt
+++ app/src/main/java/ru/abrus/androidanimations/ui/property/PropertyAnimActivity.kt
@@ -1,14 +1,19 @@
 package ru.abrus.androidanimations.ui.property
 
+import android.animation.Animator
 import android.animation.AnimatorInflater
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import androidx.core.animation.addListener
 import kotlinx.android.synthetic.main.activity_property_anim.*
 import ru.abrus.androidanimations.R
 
 class PropertyAnimActivity : AppCompatActivity() {
+    companion object{
+        private const val TAG = "PropertyAnimation"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_anim)
@@ -20,12 +25,25 @@ class PropertyAnimActivity : AppCompatActivity() {
         rotateBtn.setOnClickListener { animate(R.animator.rotate) }
 
         translateBtn.setOnClickListener { animate(R.animator.translate) }
+    }
 
+    private fun Animator.initListener() {
+        addListener({
+            Log.i(TAG, "initListener: onEnd")
+        }, {
+            Log.i(TAG, "initListener: onStart")
+        }, {
+            Log.i(TAG, "initListener: onCancel")
+        }, {
+            Log.i(TAG, "initListener: onRepeat")
+        })
     }
 
     private fun animate(id: Int) {
-        val animator = AnimatorInflater.loadAnimator(this, id)
-        animator.setTarget(globeImage)
-        animator.start()
+        AnimatorInflater.loadAnimator(this, id).apply {
+            setTarget(globeImage)
+            initListener()
+            start()
+        }
     }
 }
